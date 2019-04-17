cc7b14c8793149e14370787605a0a04d8e1798ac
Raphael Michel <mail@raphaelmichel.de>
2019-04-17 18:38:04 +0200

    ...

--- src/plum/core/models/auth.py
+++ src/plum/core/models/auth.py
@@ -10,17 +10,16 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
     email = models.EmailField(_('email address'), blank=True, unique=True)
 
-    def _create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        user = User(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
     @staticmethod
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(email, password=None, **extra_fields):
+        def _create_user(email, password, **extra_fields):
+            if not email:
+                raise ValueError('The given email must be set')
+            email = self.normalize_email(email)
+            user = User(email=email, **extra_fields)
+            user.set_password(password)
+            user.save(using=self._db)
+            return user
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+        return _create_user(email, password, **extra_fields)
