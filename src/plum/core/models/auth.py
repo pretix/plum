af8659d3a59dc9d2de512df2f45c3ed8dd31a732
Raphael Michel <michel@rami.io>
2024-03-28 17:52:35 +0100

    Fix craetesuperuser

--- src/plum/core/models/auth.py
+++ src/plum/core/models/auth.py
@@ -1,7 +1,42 @@
 # users/models.py
+from django.apps import apps
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -9,6 +44,8 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     email = models.EmailField(_('email address'), blank=True, unique=True)
+
+    objects = CustomUserManager()
 
     @staticmethod
     def create_user(email, password=None, **extra_fields):
@@ -20,6 +57,7 @@ class User(AbstractUser):
             user.set_password(password)
             user.save()
             return user
+
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return _create_user(email, password, **extra_fields)
