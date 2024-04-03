54835ebd9ce209f13689557c27c2511b861e69b5
Raphael Michel <michel@rami.io>
2024-04-03 15:56:19 +0200

    Simple 2FA implementation (#4)

--- src/plum/urls.py
+++ src/plum/urls.py
@@ -1,7 +1,9 @@
+from decorator_include import decorator_include
 from django.conf import settings
 from django.urls import path, re_path
 from django.conf.urls.static import static
 from django.urls import include
+from multifactor.decorators import multifactor_protected
 
 from .core.admin import site
 from .front import urls as fronturls
@@ -9,8 +11,9 @@ from .download import urls as dlurls
 from .upload import urls as upurls
 
 urlpatterns = [
-    re_path(r'^admin/', site.urls),
+    path('multifactor/', include('multifactor.urls')),
+    re_path(r'^admin/', decorator_include(multifactor_protected(factors=1, advertise=False), site.urls)),
     path('', include((dlurls, "download"))),
     path('', include((upurls, "upload"))),
-    path('', include((fronturls, "front"))),
+    path('', decorator_include(multifactor_protected(factors=0, advertise=False), (fronturls, "front"))),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
